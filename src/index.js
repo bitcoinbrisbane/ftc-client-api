36fb2c8ce72e5117fbea64769d7eb04afb418c22
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-04-12 14:01:28 +1000

    chore: add stubs

--- src/index.js
+++ src/index.js
@@ -14,9 +14,43 @@ app.get("/user/:id", (req, res) => {
     const user = {
         id,
         name: "John Doe",
+        brc: "2023/1234",
     };
 
     res.send(user);
+});
+
+app.get("/services/", (req, res) => {
+    const items = [{
+        id: 123,
+        date: "2023/1/1",
+        type: "Phone Call",
+        price: 45.00,
+        tax: 4.50,
+        total: 49.50,
+    }];
+
+    res.send(items);
+});
+
+app.get("/interactions/", (req, res) => {
+    const items = [{
+        id: 123,
+        date: "2023/1/1",
+        type: "Email Subject"
+    }];
+
+    res.send(items);
+});
+
+app.get("/documents/", (req, res) => {
+    const items = [{
+        id: 123,
+        date: "2023/1/1",
+        type: "Email Subject"
+    }];
+
+    res.send(items);
 });
 
 app.listen(port, () => console.log(`FTC api listening on port ${port}!`));
